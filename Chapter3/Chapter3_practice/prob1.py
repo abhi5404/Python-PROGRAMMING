4ab53ea1504b6a462c7b2e4ec9b0945fe2d93142
Abhijit Mondal <abhijitamp378@gmail.com>
2025-07-08 21:34:54 +0530

    string

---
+++ Chapter3/Chapter3_practice/prob1.py
@@ -0,0 +1,3 @@
+name = input("Enter your name: ")
+
+print(f"Good Afternoon, {name} ") #Using f-string for formatted output
---
+++ Chapter3/Chapter3_practice/prob3.py
@@ -0,0 +1,3 @@
+name = "Harry is a good  boy and  "
+
+print(name.find("  "))# This will return the index of the first occurrence of "  " in the string.
