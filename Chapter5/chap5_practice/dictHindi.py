c4b5c5fe4a4236698d831e2888dd0d398b171452
Abhijit Mondal <abhijitamp378@gmail.com>
2025-07-09 12:47:31 +0530

    ps

---
+++ Chapter5/chap5_practice/dictHindi.py
@@ -0,0 +1,10 @@
+words={
+    "madad": "help",
+    "dost": "friend",
+    "pyaar": "love",
+    "khushi": "happiness",
+    "dard": "pain",
+    
+}
+word = input("Enter a Hindi word: ")
+print(words[word])  # This will print the English meaning of the Hindi word
---
+++ Chapter5/chap5_practice/friends.py
@@ -0,0 +1,20 @@
+d = {}
+
+name = input("Enter friends name: ")
+lang = input("Enter Language name: ")
+d.update({name: lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter Language name: ")
+d.update({name: lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter Language name: ")
+d.update({name: lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter Language name: ")
+d.update({name: lang})
+
+
+print(d)# This will print the dictionary with friends' names and their favorite languages
